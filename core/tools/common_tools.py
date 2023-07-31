f90201eb61a00ba0dc3791d126b4f38c591ec316
captainfffsama <tuanzhangsama@outlook.com>
2023-07-31 16:38:25 +0800

    修复chiebot_sample_tags 可能是None 导致修改失败的情况

--- core/tools/common_tools.py
+++ core/tools/common_tools.py
@@ -449,9 +449,9 @@ def tag_chiebot_sample(
             else:
                 content=set(content)
             content |= tags
-            sample.set_field(KEY, tuple(content),dynamic=True)
+            sample.set_field(KEY, tuple(content),validate=False,dynamic=True)
         else:
-            sample.set_field(KEY, tuple(tags),dynamic=True)
+            sample.set_field(KEY, tuple(tags),validate=False,dynamic=True)
 
 
 @print_time_deco
@@ -499,4 +499,4 @@ def untag_chiebot_sample(
             else:
                 content=set(content)
             content -= tags
-            sample.set_field(KEY, tuple(content),dynamic=True)
+            sample.set_field(KEY, tuple(content),validate=False,dynamic=True)
